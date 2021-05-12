ccbd127e97392d902ea55b51efa10b785230845b
Maximilian Röll <maximilian.roell@plentymarkets.com>
2021-05-12 11:36:01 +0200

    add category crit css test

--- critCssGen.js
+++ critCssGen.js
@@ -1,6 +1,6 @@
 const critical = require('critical');
 critical.generate({
-  src: 'https://ceres-demo.plentymarkets-cloud01.com/',
+  src: 'https://ceres-demo.plentymarkets-cloud01.com/wohnzimmer',
   target: './crit.css',
   width: 1920,
   height: 1080,
