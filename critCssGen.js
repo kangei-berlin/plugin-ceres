1f53e271499077e38f037cbe686a4ec4d3774434
Maximilian Röll <maximilian.roell@plentymarkets.com>
2021-05-12 10:50:21 +0200

    added crit css from js gen tool

---
+++ critCssGen.js
@@ -0,0 +1,10 @@
+const critical = require('critical');
+critical.generate({
+  src: 'https://ceres-demo.plentymarkets-cloud01.com/',
+  target: './crit.css',
+  width: 1920,
+  height: 1080,
+  inline: true
+}, (err, criticalCSS) => {
+  console.log(criticalCSS);
+});
